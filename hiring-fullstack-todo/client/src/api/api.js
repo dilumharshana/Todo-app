f5346ef9d4348ba916e14d766193eda368329c3a
dilumharshana <dilum.harshana123@gmail.com>
2026-03-05 19:53:34 +0530

    improvement - integrated get all todos end point

--- hiring-fullstack-todo/client/src/api/api.js
+++ hiring-fullstack-todo/client/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+console.log(baseURL);
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: baseURL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/jsn',
   },
--- hiring-fullstack-todo/client/src/services/todoServices.js
+++ hiring-fullstack-todo/client/src/services/todoServices.js
@@ -1,4 +1,5 @@
 import apiClient from '../api/api';
 
 export const todoService = {
+    getAll: () => apiClient.get('/todos'),
 };
